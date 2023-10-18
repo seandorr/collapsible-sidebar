eeb39edcaec35b924cbbde55d9502f634bd18ddb
Sean Dorr <seanmichael.dorr@gmail.com>
2023-10-18 22:46:27 +0200

    fix: defaultDark overriding light theme feat: primary color setting

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import "./styles/main.scss";
 
 export default function App() {
   const [themeName, setThemeName] = useLocalStorage("theme", "system");
-  const [theme, setTheme] = useState(defaultDark ? "dark" : "light");
+  const [theme, setTheme] = useState(
+    themeName === "system" && defaultDark ? "dark" : "light"
+  );
+  const [primaryColor, setPrimaryColor] = useLocalStorage(
+    "primary-color",
+    "green"
+  );
 
   const [activeSidebarItem, setActiveSidebarItem] = useState(sidebarItems[0]);
   const [sidebarExpanded, setSidebarExpanded] = useState(false);
@@ -22,7 +28,7 @@ export default function App() {
   };
 
   return (
-    <div className="container" data-theme={theme}>
+    <div className="container" data-theme={theme} data-color={primaryColor}>
       <div className="wrapper">
         <div className="sidebar" style={sidebarWidth}>
           <div
@@ -81,14 +87,45 @@ export default function App() {
           <div className="content-container">
             <h1>{activeSidebarItem.label}</h1>
             {activeSidebarItem.label === "Settings" && (
-              <>
-                <p style={{ marginBottom: "16px" }}>Theme:</p>
-                <ThemeButtons
-                  themeName={themeName}
-                  setTheme={setTheme}
-                  setThemeName={setThemeName}
-                />
-              </>
+              <div
+                style={{ display: "flex", flexDirection: "column", gap: 32 }}
+              >
+                <div>
+                  <p style={{ marginBottom: "16px" }}>Theme:</p>
+                  <ThemeButtons
+                    themeName={themeName}
+                    setTheme={setTheme}
+                    setThemeName={setThemeName}
+                  />
+                </div>
+                <>
+                  <p>Primary color:</p>
+                  <div className="theme-btn-container">
+                    <button
+                      className={`${
+                        primaryColor === "green"
+                          ? "theme-btn active"
+                          : "theme-btn"
+                      }`}
+                      onClick={() => setPrimaryColor("green")}
+                      tabIndex={0}
+                    >
+                      green
+                    </button>
+                    <button
+                      className={`${
+                        primaryColor === "red"
+                          ? "theme-btn active"
+                          : "theme-btn"
+                      }`}
+                      onClick={() => setPrimaryColor("red")}
+                      tabIndex={0}
+                    >
+                      red
+                    </button>
+                  </div>
+                </>
+              </div>
             )}
           </div>
         </main>
